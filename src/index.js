a1be4ab5e8670ddbba9faed71c9ab40c86f3f1a2
Cristiano de Paula <cristiano.paula@alstratech.com>
2022-06-25 11:31:11 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class App extends React.Component {
+    // Javascript class, not React 
+    // constructor(props) {
+    //     super(props);
+    //     // lets initialize state (THIS IS THE ONLY TIME we do direct assignment do this.state )
+    //     this.state = { lat: null, errorMessage: '' };
+    // }
+
+    // equivalent to constructor because Babel will create the constructor for us
+    state = { lat: null, errorMessage: '' }; 
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            // callback (some point time in the future)
+            (position) => this.setState({ lat: position.coords.latitude }),
+            (err) => this.setState({ errorMessage: err.message })
+        );
+        console.log('My component was rendered to the screen');
+    }
+
+    componentDidUpdate() {
+        console.log('My component was just update... rerender');
+    }
+
+    // React says we have to define render!!
+    render() {
+
+        // Conditional rendering
+        if (this.state.errorMessage && !this.state.lat){
+            return  <div>Error: {this.state.errorMessage}</div>;
+        }
+        
+        if (!this.state.errorMessage && this.state.lat){
+            return  <SeasonDisplay lat={this.state.lat} />;
+        }
+
+        return <div>Loading!</div>;
+    };
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
